c002dcbc79fea1ffd6eb574e2dab5a5dc623cf19
Luis <fasterplop@gmail.com>
2026-01-20 14:14:25 -0400

    setup ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite'; // <--- OJO: Esta es la importación correcta para v4
+import react from '@astrojs/react';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://fj-cueva.com',
+  
+  // Configuración para Tailwind v4
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [react()],
+  
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  }
+});
